1e7fa1d17031e0d0ecdf893011c31570214e2589
yangyr123 <609105197@qq.com>
2018-03-18 07:31:13 +0800

    look

--- src/main/java/com/test/controller/UserInfoController.java
+++ src/main/java/com/test/controller/UserInfoController.java
@@ -2,8 +2,10 @@ package com.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.test.pojo.UserInfo;
 import com.test.service.UserInfoService;
 
 @Controller
@@ -16,6 +18,19 @@ public class UserInfoController {
 	@RequestMapping("/delete")
 	public void delete(Integer id) {
 		userInfoService.delete(id);
+	}
+	@RequestMapping("/add")
+	public void add(@RequestBody UserInfo  user) { // 实体类对象不能直接作为参数，药价@RequestBody
+		userInfoService.add(user);
+	}
+	@RequestMapping("/update")
+	public void update(@RequestBody UserInfo  user){
+		userInfoService.update(user);
+	}
+	@RequestMapping("/select")
+	public UserInfo select() {
+		UserInfo user = userInfoService.select();
+		return user;
 	}
 	
 }
