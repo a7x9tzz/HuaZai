6023c23222d3755a9c55d975d2a2ff0bcab54aa5
yangyr123 <609105197@qq.com>
2018-03-18 07:54:41 +0800

    look

---
+++ src/main/java/com/test/pojo/Menu.java
@@ -0,0 +1,33 @@
+package com.test.pojo;
+
+public class Menu {
+	private Integer id;
+	private String name;
+	private String style;
+	private Integer price;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStyle() {
+		return style;
+	}
+	public void setStyle(String style) {
+		this.style = style;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+
+}
