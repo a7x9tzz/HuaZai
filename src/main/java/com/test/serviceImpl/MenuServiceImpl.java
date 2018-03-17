b36782fccc82605a27cbd8962cca50b3920abedb
yangyr123 <609105197@qq.com>
2018-03-18 07:57:49 +0800

    look

---
+++ src/main/java/com/test/serviceImpl/MenuServiceImpl.java
@@ -0,0 +1,40 @@
+package com.test.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.test.dao.MenuDao;
+import com.test.pojo.Menu;
+import com.test.service.MenuService;
+
+@Service("menuService")
+public class MenuServiceImpl implements MenuService {
+	
+	@Autowired
+	private MenuDao menuDao;
+	
+	
+	public void add(Menu menu) {
+		menuDao.add(menu);
+		
+	}
+
+
+	public void delete(Integer id) {
+		menuDao.delete(id);
+		
+	}
+
+
+	public void update(Menu menu) {
+		menuDao.update(menu);
+		
+	}
+
+
+	public Menu select() {
+		Menu menu = menuDao.select();
+		return menu;
+	}
+
+}
