fa09b106e9cee10b26f61fcd06fa38b1f2a453c6
yangyr123 <609105197@qq.com>
2018-03-18 07:44:13 +0800

    look

--- src/main/java/com/test/serviceImpl/UserInfoServiceImpl.java
+++ src/main/java/com/test/serviceImpl/UserInfoServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.test.dao.UserInfoDao;
+import com.test.pojo.UserInfo;
 import com.test.service.UserInfoService;
 
 @Service("userInfoService")
@@ -12,9 +13,31 @@ public class UserInfoServiceImpl implements UserInfoService {
 	@Autowired
 	private UserInfoDao userInfoDao;
 	
-	@Override
+	
+
+	public void add(UserInfo user) {
+		userInfoDao.add(user);
+	}
+
+
+
 	public void delete(Integer id) {
 		userInfoDao.delete(id);
+		
+	}
+
+
+
+	public void update(UserInfo user) {
+		userInfoDao.update(user);
+		
+	}
+
+
+
+	public UserInfo select() {
+		UserInfo user = userInfoDao.select();
+		return user;
 	}
 
 }
