f8ffe3f054d5c7ba060b651acd7b0f0ef9b6891b
yangyr123 <609105197@qq.com>
2018-03-18 07:42:28 +0800

    look

--- src/main/java/com/test/service/UserInfoService.java
+++ src/main/java/com/test/service/UserInfoService.java
@@ -1,5 +1,10 @@
 package com.test.service;
 
+import com.test.pojo.UserInfo;
+
 public interface UserInfoService {
 	public void delete(Integer id);
+	public void add(UserInfo user);
+	public void update(UserInfo user);
+	public UserInfo select();
 }
